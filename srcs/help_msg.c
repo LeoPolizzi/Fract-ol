e122e5cd20033b587e7b5a917abed44377a54dd5
Leo Polizzi <lpolizzi@c2r9p2.42nice.fr>
2024-12-06 08:18:12 +0100

    Changed help message for normal version to remove bonus controls.

--- srcs/help_msg.c
+++ srcs/help_msg.c
@@ -60,7 +60,6 @@ void	print_controls(void)
 	ft_putendl_fd("\n+====================  Controls  ====================+",
 		1);
 	ft_putendl_fd("WASD or arrow keys\t\tmove view.", 1);
-	ft_putendl_fd("Space bar\t\t\tchange colors.", 1);
 	ft_putendl_fd("Scroll wheel\t\t\tzoom in and out.", 1);
 	ft_putendl_fd("ESC or close window\t\tquit fract'ol.", 1);
 	ft_putendl_fd("+====================================================+\n",
